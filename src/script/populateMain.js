50f96016ace715932701ec7d6c0ad4cc9a29f590
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2024-01-04 13:55:28 +0100

    Working on the new project form it now pops up and I have created the inputs. It will need some class and ID assingnement and then some CSS styling so that it looks pretty

--- src/script/populateMain.js
+++ src/script/populateMain.js
@@ -39,7 +39,7 @@ export default function populateMain(clickedElementId, projectTitle) {
 
     } else if (clickedElementId === 'taskButton') {
         const taskCards = initializeTasks(projectTitle)
-          taskCards.forEach(task => {
+        taskCards.forEach(task => {
             mainElement.appendChild(task)
             console.log(task)
         })
@@ -47,28 +47,80 @@ export default function populateMain(clickedElementId, projectTitle) {
     return mainElement;
 };
 
-
-
-
-
-export function createProjectButton(){
+export function createProjectButton() {
+    // Access existing elements
+    const dialogElement = document.querySelector('dialog');
     // Create button element
-    const projectButtonElement = document.createElement('input')
+    const projectButtonElement = document.createElement('input');
     // Choosing element type
-    projectButtonElement.type = 'button'
+    projectButtonElement.type = 'button';
     // Adding content to button
-    projectButtonElement.value = 'Create new Project'
+    projectButtonElement.value = 'Create new Project';
     // Adding classes to element
-    projectButtonElement.classList.add('button')
+    projectButtonElement.classList.add('button');
     // Adding ID's
-    projectButtonElement.id = 'projectButton'
+    projectButtonElement.id = 'projectButton';
 
-    projectButtonElement.addEventListener('click', )
-
-
+    projectButtonElement.addEventListener('click', () => {
+        dialogElement.textContent = '';
+        dialogElement.appendChild(createNewProject());
+        dialogElement.showModal();
+    });
     return projectButtonElement
-}
+};
 
-function createNewProject(){
-    
+function createNewProject() {
+
+    // Create form
+    const formElement = document.createElement('form');
+    // Create form children
+    const formTitleElement = document.createElement('h1');
+
+    const formProjectTitlelabelElement = document.createElement('label');
+    const formProjectTitleInputElement = document.createElement('input');
+
+    const formProjectDesclabelElement = document.createElement('label');
+    const formProjectDescInputElement = document.createElement('input');
+
+    const formProjectDueDatelabelElement = document.createElement('label');
+    const formProjectDueDateInputElement = document.createElement('input');
+
+    const formProjectPrioritylabelElement = document.createElement('label');
+    const formProjectPriorityInputElement = document.createElement('input');
+
+    const formSubmitButton = document.createElement('input');
+
+    // Choosing element type
+    formProjectDueDateInputElement.type = 'date';
+    formSubmitButton.type = 'button';
+
+
+    // Adding content to elements
+    formTitleElement.textContent = 'Create your new project.';
+    formProjectTitlelabelElement.textContent = 'Project Title: ';
+    formProjectDesclabelElement.textContent = 'Project Description: ';
+    formProjectDueDatelabelElement.textContent = 'Project Due Date: ';
+    formProjectPrioritylabelElement.textContent = 'Project Priority: ';
+    formSubmitButton.value = 'Submit new project'
+
+
+    // Add children to formElement
+    formElement.appendChild(formTitleElement);
+
+    formElement.appendChild(formProjectTitlelabelElement);
+    formElement.appendChild(formProjectTitleInputElement);
+
+    formElement.appendChild(formProjectDesclabelElement);
+    formElement.appendChild(formProjectDescInputElement);
+
+    formElement.appendChild(formProjectDueDatelabelElement);
+    formElement.appendChild(formProjectDueDateInputElement);
+
+    formElement.appendChild(formProjectPrioritylabelElement);
+    formElement.appendChild(formProjectPriorityInputElement);
+
+    formElement.appendChild(formSubmitButton)
+
+    return formElement;
+
 }
