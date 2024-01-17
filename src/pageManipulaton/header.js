36486f67d711e35ec15b009a6a14ad300d9f71ce
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2024-01-17 11:34:41 +0100

    Added a light and dark mode toggle I used :root and :root.special in the css and a toggle button in the head

--- src/pageManipulaton/header.js
+++ src/pageManipulaton/header.js
@@ -4,9 +4,21 @@ export default function createHeader(){
     const headerElement = document.createElement('header');
     // Create children for header element
     const headingElement = document.createElement('h1')
+    const themeToggle = document.createElement('button')
     // Add content to children
     headingElement.innerHTML = `It's time to get things done`
+    themeToggle.value = 'theme'
+    // Add event listener to button
+    themeToggle.addEventListener('click', () =>{
+        toggleTheme()
+    })
+
     // Populate main
     headerElement.appendChild(headingElement)
+    headerElement.appendChild(themeToggle)
     return headerElement
 };
+
+function toggleTheme (){
+    document.documentElement.classList.toggle("special")
+}
