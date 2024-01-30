b7caa53a7a0088414e9b7ed22634a30e414d70d6
JustDom223 <124432802+JustDom223@users.noreply.github.com>
2024-01-30 11:07:08 +0100

    working on populate main to remove dependency cycles and refactoring to implement the new "activateButtons" function as there were eventlisteners being added all over the place

--- src/script/populateMain.js
+++ src/script/populateMain.js
@@ -1,79 +1,78 @@
 import initializeProjects from "./initializeProjects";
 import initializeTasks from "./initializeTasks";
-import createNewProjectForm from "./createNewProject";
-import createNewTaskForm from "./createNewTask";
-import { getProjects } from "./projects";
-import createSVGImg from "./createSVGImg";
-import tasksSVG from "../assets/svg/clipboard-add-svgrepo-com.svg";
-import projectSVG from "../assets/svg/rocket-svgrepo-com.svg";
+import { createProjectButton, createTaskButton  } from "./createButtons";
+import activateButtons from "./activateButtons";
 
-export function createTaskButton(projectTitle) {
-  // Access existing elements
-  const selectedProject = getProjects().find(
-    (project) => project.title === projectTitle,
-  );
-  const dialogElement = document.querySelector("dialog");
-  // Create button element
-  const taskButtonSVGElement = createSVGImg(tasksSVG, "customCursor", "taskButtonSVG");
-  // Add project title as data
-  taskButtonSVGElement.dataset.projectTitle = selectedProject.title;
+// export function createTaskButton(projectTitle) {
+//   // Access existing elements
+//   const selectedProject = getProjects().find(
+//     (project) => project.title === projectTitle,
+//   );
+//   // const dialogElement = document.querySelector("dialog");
+//   // Create button element
+//   const taskButtonSVGElement = createSVGImg(tasksSVG, "actionButton", "taskButtonSVG");
+//   // Add project title as data
+//   taskButtonSVGElement.dataset.projectTitle = selectedProject.title;
+//   taskButtonSVGElement.dataset.action = "createTask";
 
-  taskButtonSVGElement.addEventListener("click", () => {
-    dialogElement.textContent = "";
-    dialogElement.appendChild(createNewTaskForm());
-    dialogElement.showModal();
-  });
-  return taskButtonSVGElement;
-}
+//   // taskButtonSVGElement.addEventListener("click", () => {
+//   //   dialogElement.textContent = "";
+//   //   dialogElement.appendChild(createNewTaskForm());
+//   //   dialogElement.showModal();
+//   // });
 
-export function createProjectButton() {
-  const projectSVGElement = createSVGImg(projectSVG, "customCursor", "projectSVG");
-  // Access existing elements
-  const dialogElement = document.querySelector("dialog");
-  // Create button element
-  projectSVGElement.addEventListener("click", () => {
-    dialogElement.textContent = "";
-    dialogElement.appendChild(createNewProjectForm());
-    dialogElement.showModal();
-  });
-  return projectSVGElement;
-}
+//   return taskButtonSVGElement;
+// }
 
-export default function populateMain(clickedElementId, projectTitle) {
+// export function createProjectButton() {
+//   const projectSVGElement = createSVGImg(projectSVG, "customCursor", "projectSVG");
+//   // Access existing elements
+//   const dialogElement = document.querySelector("dialog");
+//   // Create button element
+//   projectSVGElement.addEventListener("click", () => {
+//     dialogElement.textContent = "";
+//     dialogElement.appendChild(createNewProjectForm());
+//     dialogElement.showModal();
+//   });
+//   return projectSVGElement;
+// }
+
+export default function populateMain(clickedElementActionData, projectTitle) {
   const mainElement = document.querySelector("main");
   // Clear main
   mainElement.textContent = "";
 
   // Adding content to main elements
 
-  if (clickedElementId === "projects") {
+  if (clickedElementActionData === "projects") {
     const newProjectButton = createProjectButton();
     const toDoCards = initializeProjects();
     toDoCards.forEach((project) => {
       mainElement.appendChild(project);
     });
     mainElement.appendChild(newProjectButton);
-  } else if (clickedElementId === "today") {
+  } else if (clickedElementActionData === "today") {
     const toDoCards = initializeProjects();
     toDoCards.forEach((project) => {
       mainElement.appendChild(project);
     });
-    //   } else if (clickedElementId === 'week') {
+    
+    //   } else if (clickedElementActionData === 'week') {
     //     const toDoCards = initializeWeek();
     //     toDoCards.forEach((project) => {
     //       mainElement.appendChild(project);
     //     });
-    //   } else if (clickedElementId === 'month') {
+    //   } else if (clickedElementActionData === 'month') {
     //     const toDoCards = initializeMonth();
     //     toDoCards.forEach((project) => {
     //       mainElement.appendChild(project);
     //     });
-    //   } else if (clickedElementId === 'quater') {
+    //   } else if (clickedElementActionData === 'quater') {
     //     const toDoCards = initializeQuater();
     //     toDoCards.forEach((project) => {
     //       mainElement.appendChild(project);
     //     });
-  } else if (clickedElementId === "taskButton") {
+  } else if (clickedElementActionData === "taskButton") {
     const projectHeading = document.createElement("h1");
     projectHeading.innerText = projectTitle;
     const taskCards = initializeTasks(projectTitle);
@@ -83,5 +82,6 @@ export default function populateMain(clickedElementId, projectTitle) {
     const newTaskButton = createTaskButton(projectTitle);
     mainElement.appendChild(newTaskButton);
   }
+  activateButtons();
   return mainElement;
 }
